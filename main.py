4b30d1f3b197c59d31a6698f076481fdf05ea290
Олег <mishin415@yandex.ru>
2023-03-10 00:35:50 +0300

    Homework3.1

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Введите порядковый номер месяца")
+x = {1: "January 31 days", 2: "February  28 days", 3: "March 31 days",
+    4: "April 30 days", 5: "May 31 days", 6: "June 30 days", 7: "July 31 days",
+    8: "August 31 days", 9: "September 30 days", 10: "October 31 days",
+    11: "November 30 days", 12: "December 31 days", 13: "Нет такого месяца"}
+print(x[int(input())])
+
+
